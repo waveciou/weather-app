940f9f58b18bd2beedd82284923c54cb7de12c60
丘惟夫 <waveciou@gmail.com>
2022-06-04 05:16:20 +0800

    [dev] set barchart flow

--- src/ts/barChart.tsx
+++ src/ts/barChart.tsx
@@ -5,35 +5,57 @@ interface IBarChartProps {
   amount: number[];
 }
 
+interface IAmountData {
+  value: number;
+  percent: number;
+}
+
 const BarChart: React.FC<IBarChartProps> = ({ amount }) => {
-  const [amountList, setAmountList] = useState<number[]>([]);
+  const [amountList, setAmountList] = useState<IAmountData[]>([]);
 
   useEffect(() => {
-    const count = amount.reduce((p, c) => p + Math.abs(c), 0);
-    const result = amount.map((value) => Math.floor((value / count) * 100));
+    const max: number = Math.max(...amount);
+    const min: number = Math.min(...amount);
+    const range: number = max - min;
+
+    const result: IAmountData[] = amount.map((value) => {
+      let percent = 100;
+
+      if (value === max) {
+        percent = 90;
+      } else if (value === min) {
+        percent = 10;
+      } else {
+        percent = (value - min) / range * 100;
+      }
+
+      return { value: Math.floor(value), percent };
+    });
+
     setAmountList(result);
   }, [amount]);
 
   return (
     <svg className="bar-chart" width="300px" height="100%" y="0px" x="0px">
       {
-        amountList.map((value, index) => {
+        amountList.map(({ value, percent }, index: number) => {
           const id: string = uuidv4();
           return (
             <g key={id}>
               <rect
                 className="bar-chart__column"
                 width="50px"
-                height={`${value}%`}
-                y={`${100 - value}%`}
-                x={`${(50 + 12.5) * index}px`}
+                height={`${percent}%`}
+                y={`${100 - percent}%`}
+                x={`${(50 + 12) * index}px`}
               ></rect>
               <text
-                x={`${(50 + 12.5) * index + 25}px`}
-                y={`${100 - value}%`}
-                fill="#35495e"
+                x={`${(50 + 12) * index + 25}px`}
+                y={`${104 - percent}%`}
+                fill="#ffffff"
                 alignmentBaseline="central"
                 textAnchor="middle"
+                fontSize="14"
               >{ value }</text>
             </g>
           );
